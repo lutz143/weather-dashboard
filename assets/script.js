9f8ed9e214d8a7f8bace277a712e87f677fd420f
Josh <lutz_143@yahoo.com>
2022-11-25 12:33:32 -0500

    added styling and code to hide and unhide search bar

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,38 @@
 console.log('up and ready');
 
-$(document).ready(function() {
-  $("#search-button").on("click", function(){
-    console.log('side bar button clicked!');
-    var searchBarEl = document.querySelector('.hide');
-    if (searchBarEl.style.display = "none"){
-      console.log('should show');
-      searchBarEl.removeAttribute("class", "hide");
-      searchBarEl.setAttribute("style", "display: flex");
-    } else if (searchBarEl.style.display = null) {
-      searchBarEl.setAttribute("style", "display: none");
-    }
+$("#search-button").on("click", function(){
+  console.log('side bar button clicked!');
+  var searchBarEl = document.querySelector('.search-bar');
+  var hideEl = document.querySelector('.hide');
 
-  })
+  if(searchBarEl.style.display !=="none") {
+    searchBarEl.style.display = "none";
+    hideEl.style.display = "none";
+  } else {
+    searchBarEl.style.display = "block";
+    hideEl.style.display = "flex";
+  }
 })
 
 // $(document).ready(function() {
 //   $("#search-button").on("click", function(){
 //     console.log('side bar button clicked!');
 //     var searchBarEl = document.querySelector('.search-bar');
-//     if (searchBarEl.style.display = "none") {
-//       searchBarEl.style.display = "flex";
-//     } else {
+//     var hideEl = document.querySelector('.hide');
+
+//     if(searchBarEl.style.display !=="none") {
 //       searchBarEl.style.display = "none";
+//       hideEl.style.display = "none";
+//     } else {
+//       searchBarEl.style.display = "block";
+//       hideEl.style.display = "flex";
 //     }
 //   })
 // })
 
+function handleFormSubmit(event) {
+  event.preventDefault();
 
-// function showSearchBar() {
-//   var searchBarEl = document.getElementById("search-bar");
-//   if (searchBarEl.style.display === "none") {
-//     searchBarEl.style.display = "block";
-//   } else {
-//     searchBarEl.style.display = "none";
-//   }
-// }
+  $(function () {
+  });
+}
