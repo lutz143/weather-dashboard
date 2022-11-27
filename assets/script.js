cacf40ecb4a47bea17d7376166f2f1faf2840281
Josh <lutz_143@yahoo.com>
2022-11-27 13:39:27 -0500

    added clear button and code to clear localStorage

--- assets/script.js
+++ assets/script.js
@@ -14,21 +14,26 @@ $("#search-button").on("click", function(){
   }
 })
 
-// $(document).ready(function() {
-//   $("#search-button").on("click", function(){
-//     console.log('side bar button clicked!');
-//     var searchBarEl = document.querySelector('.search-bar');
-//     var hideEl = document.querySelector('.hide');
+$(document).ready(function() {
+  $(".search-icon").on("click", function(event){
+    event.preventDefault();
+    console.log('search button clicked!');
+    var searchCity = $(".search-bar").val();
+    var searchCityState = 'Ohio';
+    console.log(searchCity);
+    // var task = $(this).siblings('#taskDescription').val();
 
-//     if(searchBarEl.style.display !=="none") {
-//       searchBarEl.style.display = "none";
-//       hideEl.style.display = "none";
-//     } else {
-//       searchBarEl.style.display = "block";
-//       hideEl.style.display = "flex";
-//     }
-//   })
-// })
+    localStorage.setItem(searchCity, searchCityState);    
+  })
+})
+
+$(document).ready(function() {
+  $("#clear-button").on("click", function(){
+    localStorage.clear();
+    location.reload();
+  })
+})
+
 
 function handleFormSubmit(event) {
   event.preventDefault();
