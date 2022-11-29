bafc80ba978774833da96186a88a99d4f8f9fdaa
Josh <lutz_143@yahoo.com>
2022-11-28 21:51:44 -0500

    created initial fetch with example api url to console log options

--- assets/script/fetch.js
+++ assets/script/fetch.js
@@ -1,5 +1,8 @@
 console.log("fetch up and ready");
 
+var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=e03ebc5edf6a5fc04467ffe0e3c896b7';
+
+
 // capture the value of the saved city in the sidebar if clicked
 $(document).ready(function() {
   $("[data-id]").parent().click(function() {
@@ -7,3 +10,21 @@ $(document).ready(function() {
     console.log(test);
   });
 });
+
+function getApi() {
+  fetch(requestUrl)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+    })
+  
+}
+
+getApi();
+
+// function init() {    
+// }
+
+// init();
